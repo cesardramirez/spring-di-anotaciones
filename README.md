e2c8fc835c8c24a72bc9e522b6e1044d7dd98467
cesardramirez <cesardavid89@gmail.com>
2018-12-05 22:04:38 -0500

    feat(basic): Proyecto Inicial Funcional.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # spring-di-anotaciones
 Udemy - Spring Framework 4.3 &amp; Hibernate
+
+## Descripción
+Proyecto Spring Stand-Alone con DI (Inyección de Dependencia) utilizando anotaciones.
+- Los beans (persona y direcciones) se definen a través de anotaciones.
+- Los beans (listaDeDirecciones y sus referencias (implementaciones) son direccionChile y direccionUsa) se definen desde el XML. 
+- Los valores de sus atributos se definen en el XML.
+- Se imprime un mensaje en la terminal.
--- src/main/java/com/udemy/springlocal/desafio/Main.java
+++ src/main/java/com/udemy/springlocal/desafio/Main.java
@@ -1,7 +1,24 @@
 package com.udemy.springlocal.desafio;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
-    public static void main( String[] args) {
-        System.out.println( "Hello World!" );
+    
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        BeanFactory factory = new ClassPathXmlApplicationContext("/beans.xml");
+        Persona persona = (Persona) factory.getBean("persona");
+        System.out.println(getInfoPersona(persona));
+    }
+    
+    public static String getInfoPersona(Persona persona) {
+        return "Nombre: " + persona.getNombre() + "\n" + "Edad: "
+                + persona.getEdad() + "\n" + "Peso: " + persona.getPeso()
+                + "\n" + "Es Programador?: " + persona.isProgramador() + "\n"
+                + "Direccion: " + persona.getDireccion().getNumero() + " "
+                + persona.getDireccion().getCalle() + " "
+                + persona.getDireccion().getCiudad() + " "
+                + persona.getDireccion().getPais();
     }
 }
